99254841ded2e96ed0ec53ba0971778a991903df
雅基 高野 <220034@jc-21.jp>
2024-02-05 09:45:37 +0900

    全然テクスチャが表示されない

--- TestScene.cpp
+++ TestScene.cpp
@@ -2,6 +2,7 @@
 #include "Engine/Input.h"
 #include "Engine/SceneManager.h"
 #include "Stage.h"
+#include "transDice.h"
 #include "Engine/Camera.h"
 
 TestScene::TestScene(GameObject* parent)
@@ -12,6 +13,7 @@ TestScene::TestScene(GameObject* parent)
 void TestScene::Initialize()
 {
 	Instantiate<Stage>(this);
+	//Instantiate<transDice>(this);
 }
 
 void TestScene::Update()
